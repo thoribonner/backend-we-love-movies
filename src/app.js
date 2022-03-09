e4dc081e48220e9d0bd9fa3fbb54090a22490775
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-03-08 20:47:53 -0500

    files and errors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+const notFound = require("./errors/notFound");
+const errorhandler = require("./errors/errorHandler");
+
+app.use("/movies", moviesRouter);
+app.use("reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
+
+app.use(notFound);
+app.use(errorhandler);
 
 module.exports = app;
